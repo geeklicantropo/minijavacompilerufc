89c18a58963b715c9ad10095094bb64c72f815d0
filipebarcos <filipebarcos@1ca45d7c-2172-11df-8298-5f372d8d1cf2>
2010-06-02 02:28:12 +0000

    Tabela de simbolos

---
+++ MiniJavaCompiler/src/br/ufc/compiladores/tabeladesimbolos/Bucket.java
@@ -0,0 +1,14 @@
+package br.ufc.compiladores.tabeladesimbolos;
+
+public class Bucket {
+
+	String key;
+	Object binding;
+	Bucket next;
+
+	Bucket(String k, Object b, Bucket n) {
+		key = k;
+		binding = b;
+		next = n;
+	}
+}
